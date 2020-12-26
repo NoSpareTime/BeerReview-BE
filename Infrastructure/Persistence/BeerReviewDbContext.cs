7a9cba2fae60bbbc6c4720bde1a67d6f892a6c3d
Markus Kristian Simonsen <markuskristiansimonsen@gmail.com>
2020-12-26 01:00:17 +0100

    wip

---
+++ Infrastructure/Persistence/BeerReviewDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class BeerReviewDbContext : DbContext
+    {
+        public DbSet<Beer> Beers { get; set; }
+
+        public BeerReviewDbContext(DbContextOptions<BeerReviewDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Infrastructure/Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BeerReviewDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(BeerReviewDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
